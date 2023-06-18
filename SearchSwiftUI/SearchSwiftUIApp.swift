92d02287a55971acb6d146aa67520fd74222f1d7
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-06-18 10:37:33 +0500

    Initial Commit

---
+++ SearchSwiftUI/SearchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchSwiftUIApp.swift
+//  SearchSwiftUI
+//
+//  Created by sss on 18.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SearchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
