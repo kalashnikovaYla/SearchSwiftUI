39c0ac1e20a5d736a5fcb4d42d940fb993b49f6e
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-06-18 15:37:30 +0500

    Initial commit

--- SearchSwiftUI/ContentView.swift
+++ SearchSwiftUI/ContentView.swift
@@ -8,19 +8,94 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var searchText = ""
+    @State var isSearching = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView {
+                
+                SearchBar(searchText: $searchText, isSearching: $isSearching)
+                
+                ForEach((0..<20).filter({
+                    "\($0)".contains(searchText) || searchText.isEmpty
+            
+                }), id: \.self) { num in
+                    HStack {
+                        Text("\(num)")
+                        Spacer()
+                    }.padding()
+                    Divider()
+                        .background(Color(.systemGray))
+                        .padding(.leading)
+                }
+               
+            }
+            .navigationTitle("Food")
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .colorScheme(.dark)
+    }
+}
+
+struct SearchBar: View {
+    
+    @Binding var searchText: String
+    @Binding var isSearching: Bool
+    
+    var body: some View {
+        HStack {
+            HStack {
+                TextField("Search terms here", text: $searchText)
+                    .padding(.leading, 25)
+            }
+            .padding()
+            .background(Color(.systemGray5))
+            .cornerRadius(6)
+            .padding(.horizontal)
+            .onTapGesture {
+                isSearching = true
+            }
+            .overlay {
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                    Spacer()
+                    if isSearching {
+                        Button {
+                            searchText = ""
+                        } label: {
+                            Image(systemName: "xmark.circle.fill")
+                                .padding(.vertical)
+                        }
+                    }
+                }
+                .padding(.horizontal, 32)
+                .foregroundColor(.gray)
+            }
+            .transition(.move(edge: .trailing))
+            .animation(.spring())
+            
+            if isSearching {
+                Button {
+                    isSearching = false
+                    searchText = ""
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                } label: {
+                    Text("Cancel")
+                        .padding(.trailing)
+                        .padding(.leading, 0 )
+                }
+                .transition(.move(edge: .trailing))
+                .animation(.spring())
+            }
+            
+        }
     }
 }
